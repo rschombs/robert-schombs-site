f16e2ecf3ca8d90a02c4a3eb82a016013688a541
Robert Schombs <robertschombs@gmail.com>
2022-11-21 00:31:33 -0500

    More styling and add torch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,74 @@
 let musicPlaying = false
 
 const cornellMp3 = new Audio('./assets/audio/almamater.mp3');
+const torchSound = new Audio('./assets/audio/torch-sound.mp3')
 
 const almaMater = document.getElementById('cornell-logo');
 almaMater.addEventListener('click', (e) => {
     if (musicPlaying == false) {
         cornellMp3.play();
-        musicPlaying = true
+        musicPlaying = true;
+        playIcon.classList.toggle('fa-circle-play');
+        playIcon.classList.toggle('fa-circle-pause');
+
     } else {
         cornellMp3.pause();
         cornellMp3.currentTime = 0
         musicPlaying = false;
+        playIcon.classList.toggle('fa-circle-play');
+        playIcon.classList.toggle('fa-circle-pause');
     }
 })
 
 const cornellLogo = document.getElementById('cornell-logo');
+const playIcon = document.querySelector('.fa-circle-play')
 cornellLogo.addEventListener('mouseover', (e) => {
-    cornellLogo.classList.add('active');
+    playIcon.classList.remove('hidden');
 })
 
 cornellLogo.addEventListener('mouseout', (e) => {
-    cornellLogo.classList.remove('active');
+    playIcon.classList.add('hidden');
+})
+
+const charsheet = document.querySelector('.charsheet');
+const modal = document.querySelector('.modal');
+const bioPic = document.querySelector('#name > img')
+const fullPic = document.querySelector('#full-pic');
+
+bioPic.addEventListener('click', (e) => {
+    modal.classList.toggle('hidden-modal');
+    charsheet.classList.add('blur');
+    fullPic.classList.toggle('hidden-modal');
+    fullPic.classList.toggle('activated');
+})
+
+modal.addEventListener('click', (e) => {
+    modal.classList.toggle('hidden-modal');
+    charsheet.classList.remove('blur');
+    const targetToRemove = document.querySelector('.activated');
+    targetToRemove.classList.toggle('hidden-modal');
+    targetToRemove.classList.toggle('activated');
+})
+
+const nameBox = document.querySelector('#name-box');
+nameBox.addEventListener('click', (e) => {
+    nameBox.classList.remove('glowing');
+})
+
+const pibbClicker = document.querySelector('#beagle');
+const pibbPic = document.querySelector('#pibb-pic');
+
+pibbClicker.addEventListener('click', (e) => {
+    modal.classList.toggle('hidden-modal');
+    charsheet.classList.add('blur');
+    pibbPic.classList.toggle('hidden-modal');
+    pibbPic.classList.toggle('activated');
+})
+
+const torch = document.querySelector('#torch-out');
+const body = document.body;
+torch.addEventListener('click', (e) => {
+    torch.style.opacity = 0;
+    body.classList.add('torch-cursor');
+    torchSound.play();
 })
